c5cca894b8b2e3ee164b622ff454a656bc30b3ce
Gabriel Yoshikawa Koti <gabrielkoti@hotmail.com>
2023-01-13 09:00:56 -0300

    Commit

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -1,1 +1,20 @@
-LIST
+<script>
+
+    import { onMount } from "svelte";
+
+    import { items } from "../components/stores";
+
+    import TodoApi from "./TodoApi";
+
+    function handleNewItem(e) {}
+
+    function handleUpdate(e) {}
+
+    function handleDelete(e) {}
+
+    onMount(async () => {
+
+    $items = await TodoApi.getAll();          
+
+    });
+</script>
--- src/components/stores.js
+++ src/components/stores.js
@@ -4,14 +4,6 @@ import { writable } from "svelte/store";
 
 export const items = writable([
 
-    {
-        id: 1,
-        text: "AAA",
-        completed: false
-    },
-    {
-        id: 2,
-        text: "BBB",
-        completed: true
-    },
+    
+
 ]); 
